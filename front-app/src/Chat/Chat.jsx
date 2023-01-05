9827380f195f73745def7d6de6631bb70304b587
jethronbwira@gmail.com <jethronbwira@gmail.com>
2023-01-05 15:19:47 +0100

    feat: add functionnal sending msg by user id

--- front-app/src/Chat/Chat.jsx
+++ front-app/src/Chat/Chat.jsx
@@ -18,11 +18,7 @@ function Chat() {
    const [selectedUser, setSelectedUser] = useState()
 
 console.log(selectedUser)
-   // const urlDeBase = 'http://localhost:2707/api/getMessages/:areUserConnectedID';
-   // // const urlSendMessage = 'http://localhost:2707/api/sentMessages';
-
-
-
+   const urlDeBase = 'http://localhost:2707/api/getMessages/:areUserConnectedID';
    return (
 
       <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
@@ -82,7 +78,7 @@ console.log(selectedUser)
             <div className="chat-message">
                <div className="flex items-end justify-end">
                   <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                     <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Hier je t'ai cherché mais tu n'était pas chez toi !</span></div>
+                     <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white "> Hier je t'ai cherché mais tu n'était pas chez toi !</span></div>
                   </div>
                   <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
                </div>
